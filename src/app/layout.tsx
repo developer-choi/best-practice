b207597261fded49d7b2284a7e9b8e1bfe4ee3b0
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-17 17:27:00 +0900

    feat(style): 전역 CSS Reset 및 Noto Sans KR 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
 import {PropsWithChildren} from 'react';
+import {Noto_Sans_KR} from 'next/font/google';
+
+const notoSansKr = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
 
 export default function RootLayout({children}: PropsWithChildren) {
   return (
-    <html>
-    <body>
+    <html lang="ko">
+    <body className={notoSansKr.className}>
     {children}
     </body>
     </html>
