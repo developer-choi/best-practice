56286bcf9dce3924de89f2fe39e3d8dde33effe1
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-17 17:30:53 +0900

    최상위 레이아웃 마크업 추가

--- src/app/(service)/layout.tsx
+++ src/app/(service)/layout.tsx
@@ -1,6 +1,66 @@
+import React, {ReactNode} from 'react';
 import '@/styles/reset.css';
 import '@/styles/global.css';
+import styles from './layout.module.css';
+import BaseImage from '@/components/BaseImage';
+import PortfolioLink from '@/components/PortfolioLink';
 
-export default function RootLayout() {
-  return null;
+export interface RootLayoutProps {
+  children?: ReactNode;
+}
+
+export default function RootLayout({children}: RootLayoutProps) {
+  return (
+    <>
+      <Header/>
+      <div className={styles.layoutContainer}>
+        <Sidebar/>
+        <div className={styles.pageContainer}>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerLeft}>
+        <BaseImage src="/menu.svg" width={24} height={24} alt="Menu"/>
+        <PortfolioLink href="/" aria-label="YouTube Home">
+          <BaseImage src="/logo.svg" width={93} height={20} alt="YouTube Logo"/>
+        </PortfolioLink>
+      </div>
+    </header>
+  );
+}
+
+function Sidebar() {
+  return (
+    <aside className={styles.sidebarContainer}>
+      <nav aria-label="Main navigation">
+        <ul>
+          <li>
+            <PortfolioLink href="/" className={styles.sidebarItem}>
+              <BaseImage src="/home.svg" width={24} height={24} alt="home icon"/>
+              <span>Home</span>
+            </PortfolioLink>
+          </li>
+          <li>
+            <PortfolioLink href="/shorts" className={styles.sidebarItem}>
+              <BaseImage src="/shorts.svg" width={24} height={24} alt="shorts icon"/>
+              <span>Shorts</span>
+            </PortfolioLink>
+          </li>
+          <li>
+            <PortfolioLink href="/subscriptions" className={styles.sidebarItem}>
+              <BaseImage src="/subscriptions.svg" width={24} height={24} alt="subscription icon"/>
+              <span>Subscriptions</span>
+            </PortfolioLink>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
