7cd77f5b53e54c881da7f714363e7aada0ed7fe9
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-17 17:31:43 +0900

    feat(video): 비디오 목록 페이지 마크업 추가

--- src/api/channel.ts
+++ src/api/channel.ts
@@ -1,4 +1,4 @@
-import {ChannelInfoApiResponse} from '@/types/channel';
+import {ChannelInfoApiResponse, VideoListApiResponse} from '@/types/channel';
 
 export function getChannelInfoApi() {
   const response: ChannelInfoApiResponse = {
@@ -6,6 +6,19 @@ export function getChannelInfoApi() {
     avatar: '/channel.jpg',
     name: 'Vercel',
     subscribersCount: 110,
+  };
+
+  return {data: response};
+}
+
+export function getVideoListApi() {
+  const response: VideoListApiResponse = {
+    list: Array.from({length: 12}).map((_, i) => ({
+      id: i,
+      thumbnail: `/video${i % 2}.avif`,
+      title: `Vercel Ship 2024 Day 1: The Keynote - Video ${i + 1}`,
+      viewCount: 123,
+    }))
   };
 
   return {data: response};
