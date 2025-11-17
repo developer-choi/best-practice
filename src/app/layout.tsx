7085a68b9e840703047857a939e6647c20660146
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-17 12:41:25 +0900

    Initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,11 @@
+import {PropsWithChildren} from 'react';
+
+export default function RootLayout({children}: PropsWithChildren) {
+  return (
+    <html>
+    <body>
+    {children}
+    </body>
+    </html>
+  );
+}
