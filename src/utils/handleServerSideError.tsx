8eda3baa87c8ce80440f6a4923c658c7d2dafd09
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-19 08:31:19 +0900

    Server Side에서 페이지를 렌더링 할 때, 모든 페이지에서 공통적으로 발생할 수 있는 오류를 처리하기위한 전역 처리로직 구현

---
+++ src/utils/handleServerSideError.tsx
@@ -0,0 +1,17 @@
+import ErrorPage from '@/components/ErrorPage';
+
+// TODO 이 오류무시 부분은 이후 PR에서 삭제예정
+// eslint-disable-next-line
+export default function handleServerSideError(error: unknown) {
+  const isSomeSpecificError = false;
+
+  if (isSomeSpecificError) {
+    return (
+      <ErrorPage>임의의 API에서 공통적으로 발생할 수 있는 특정 에러에 대한 에러메시지 노출</ErrorPage>
+    );
+  }
+
+  return (
+    <ErrorPage>알 수 없는 오류가 발생했습니다. 오류가 지속될 경우, 고객센터에 문의해주세요.</ErrorPage>
+  );
+}
