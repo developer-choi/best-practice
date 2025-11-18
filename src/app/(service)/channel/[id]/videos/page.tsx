bf67142e86b6cb7023fd35ea5e10a7fd44343f67
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-19 08:31:19 +0900

    feat(error): Server-Side Rendering을 위한 전역 오류 처리 로직 구현

--- src/app/(service)/channel/[id]/videos/page.tsx
+++ src/app/(service)/channel/[id]/videos/page.tsx
@@ -3,39 +3,44 @@ import styles from './page.module.css';
 import PortfolioLink from '@/components/PortfolioLink';
 import {Metadata} from 'next';
 import {getVideoListApi} from '@/api/channel';
+import handleServerSideError from '@/utils/handleServerSideError';
 
 export const metadata: Metadata = {
   title: 'Vercel - Youtube'
 };
 
 export default async function Page({params: {id}}: {params: {id: string}}) {
-  const {data} = await getVideoListApi(id);
+  try {
+    const {data} = await getVideoListApi(id);
 
-  return (
-    <main className={styles.pageContainer}>
-      {data.list.length === 0 ? (
-        <div>비디오 목록이 없어요</div>
-      ) : (
-        <ul className={styles.listContainer}>
-          {data.list.map((video) => {
-            const href = `/watch?v=${video.id}`;
+    return (
+      <main className={styles.pageContainer}>
+        {data.list.length === 0 ? (
+          <div>비디오 목록이 없어요</div>
+        ) : (
+          <ul className={styles.listContainer}>
+            {data.list.map((video) => {
+              const href = `/watch?v=${video.id}`;
 
-            return (
-              <li key={video.id} className={styles.videoItem}>
-                <PortfolioLink href={href} className={styles.thumbnailContainer}>
-                  <BaseImage src={video.thumbnail} alt={video.title} width={336} height={188} className={styles.videoThumbnail}/>
-                </PortfolioLink>
-                <div className={styles.videoDetails}>
-                  <PortfolioLink href={href} className={styles.titleLink}>
-                    <h3 className={styles.videoTitle}>{video.title}</h3>
+              return (
+                <li key={video.id} className={styles.videoItem}>
+                  <PortfolioLink href={href} className={styles.thumbnailContainer}>
+                    <BaseImage src={video.thumbnail} alt={video.title} width={336} height={188} className={styles.videoThumbnail}/>
                   </PortfolioLink>
-                  <div className={styles.videoMetadata}>{video.viewCount} views</div>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-      )}
-    </main>
-  );
+                  <div className={styles.videoDetails}>
+                    <PortfolioLink href={href} className={styles.titleLink}>
+                      <h3 className={styles.videoTitle}>{video.title}</h3>
+                    </PortfolioLink>
+                    <div className={styles.videoMetadata}>{video.viewCount} views</div>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        )}
+      </main>
+    );
+  } catch (error) {
+    return handleServerSideError(error);
+  }
 }
