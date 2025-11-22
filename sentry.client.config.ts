9b466eb0f21d38087377ebc4a30e1b94df853996
Yu Jin Choe <forworkchoe@gmail.com>
2025-11-22 19:11:32 +0900

    feat(sentry): Next.js 애플리케이션에 Sentry 오류 모니터링 구축

---
+++ sentry.client.config.ts
@@ -0,0 +1,9 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: "https://6895395bf0a1a7eb9e33d4b61f234974@o4508301096714240.ingest.us.sentry.io/4510381991264256",
+});
